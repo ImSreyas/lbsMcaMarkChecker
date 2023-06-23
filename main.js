74ca7932f7cbb1143e59d010e98a78498a6029fd
ImSreyas <sreyassatheesh755@gmail.com>
2023-06-23 23:05:11 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,85 @@
+const questionContainer = document.querySelector(".option-container");
+for (let i = 1; i <= 120; i++) {
+  const option = document.createElement("div");
+  questionContainer.append(option);
+  option.className = "option";
+  const questionNumberContainer = document.createElement("div");
+  questionNumberContainer.className = "question-number-container";
+  questionNumberContainer.innerText = i;
+
+  const questionOptionContainer = document.createElement("div");
+  questionOptionContainer.className = "question-option-container";
+  option.append(questionNumberContainer, questionOptionContainer);
+  const opt = ["A", "B", "C", "D"];
+  opt.forEach((op) => {
+    const questionOptionInside = document.createElement("div");
+    questionOptionInside.className = "question-option-inside";
+    questionOptionContainer.append(questionOptionInside);
+    questionOptionInside.setAttribute("selected", "false");
+    questionOptionInside.addEventListener("click", (e) => {
+      const children = Array.from(questionOptionInside.parentElement.children);
+      children.forEach((ch) => {
+        if (e.target != ch) {
+          ch.setAttribute("selected", "false");
+        }
+      });
+      questionOptionInside.setAttribute(
+        "selected",
+        questionOptionInside.getAttribute("selected") === "true"
+          ? "false"
+          : "true"
+      );
+    });
+    questionOptionInside.innerText = op;
+  });
+}
+
+const clear = document.querySelector(".clear-btn");
+clear.addEventListener("click", (e) => {
+  document.querySelector(".result-container").innerText = "?/120";
+  const qo = document.querySelectorAll(".question-option-inside");
+  qo.forEach((q) => {
+    q.setAttribute("selected", "false");
+  });
+});
+
+const result = document.querySelector(".result-btn");
+result.addEventListener("click", (e) => {
+  let mark = 0;
+  const ansKey =
+    document
+      .querySelector(".part-option-container div[selected='true']")
+      .innerText.trim() === "A"
+      ? "DBAADAABADBDBCDCCBBABBDCCACDBADDDCABCADDBABABCBABAABCADBABBCADBDACBBCDCDABBBDCACABCDBBDCADCDCACACBDBDDABCBBBABCBBAADCBDA"
+      : "ABCADBABBCADBDACBBCDCDABBBDCACABCDBBDCADCDCACACBDBDDABCBBBABCBBAADCBDADBAADAABADBDBCDCCBBABBDCCACDBADDDCABCADDBABABCBABA";
+  let candidateAns = "";
+  const selectedAnsContainers = document.querySelectorAll(
+    ".question-option-container"
+  );
+  selectedAnsContainers.forEach((selectedAnsContainer, index) => {
+    const selectedOptions = Array.from(selectedAnsContainer.children);
+    selectedOptions.forEach((o) => {
+      if (o.getAttribute("selected") === "true") {
+        if (o.innerText.trim() == ansKey[index]) {
+          mark = mark + 1;
+        }
+      }
+    });
+  });
+  document.querySelector(".result-container").innerText = `${mark}/120`;
+});
+
+const partOptions = Array.from(
+  document.querySelectorAll(".part-option-container div")
+);
+partOptions.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    partOptions.forEach((o) => {
+      o.setAttribute("selected", "false");
+    });
+    option.setAttribute(
+      "selected",
+      option.getAttribute("selected") === "true" ? "false" : "true"
+    );
+  });
+});
